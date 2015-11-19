78f9546dfcd78b5030df21b07a08458072238907
djragsdale <djragsdale@anderson.edu>
2015-11-19 00:44:05 -0500

    Added sample data and fixed exports.post(s) causing error.

--- routes/api.js
+++ routes/api.js
@@ -10,10 +10,138 @@ var data = {
             "title": "Sed egestas",
             "text": "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus."
         }
+    ],
+    "lists": [
+        {
+            "id": 1,
+            "title": "Monday 1",
+            "lastupdated": "11/19/2015 12:26 AM",
+            "items": [
+                {
+                    "seqno": 1,
+                    "checked": false,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 1"
+                },
+                {
+                    "seqno": 2,
+                    "checked": true,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 2"
+                },
+                {
+                    "seqno": 3,
+                    "checked": false,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 3"
+                },
+                {
+                    "seqno": 4,
+                    "checked": false,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 4"
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "title": "Monday 2",
+            "lastupdated": "11/19/2015 12:26 AM",
+            "items": [
+                {
+                    "seqno": 1,
+                    "checked": false,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 1"
+                },
+                {
+                    "seqno": 2,
+                    "checked": false,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 2"
+                },
+                {
+                    "seqno": 3,
+                    "checked": true,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 3"
+                },
+                {
+                    "seqno": 4,
+                    "checked": false,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 4"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "title": "Monday 3",
+            "lastupdated": "11/19/2015 12:26 AM",
+            "items": [
+                {
+                    "seqno": 1,
+                    "checked": true,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 1"
+                },
+                {
+                    "seqno": 2,
+                    "checked": true,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 2"
+                },
+                {
+                    "seqno": 3,
+                    "checked": true,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 3"
+                },
+                {
+                    "seqno": 4,
+                    "checked": true,
+                    "updated": "11/19/2015 12:26 AM",
+                    "text": "Item 4"
+                }
+            ]
+        }
+    ],
+    "templates": [
+        {
+            "id": 1,
+            "title": "Monday",
+            "lastupdated": "11/19/2015 12:20 AM",
+            "items": [
+                {
+                    "text": "Item 1"
+                },
+                {
+                    "text": "Item 2"
+                },
+                {
+                    "text": "Item 3"
+                },
+                {
+                    "text": "Item 4"
+                }
+            ]
+        }
     ]
 };
 
 // GET
+
+exports.lists = function (req, res) {
+    var lists = [];
+    data.lists.forEach(function (list, i) {
+        lists.push({
+            id: i,
+            title: list.title,
+            lastupdated: list.lastupdated,
+            items: list.items
+        });
+    });
+};
 
 exports.posts = function (req, res) {
     var posts = [];
@@ -29,7 +157,7 @@ exports.posts = function (req, res) {
     });
 };
 
-exports.posts = function (req, res) {
+exports.post = function (req, res) {
     var id = req.params.id;
     if (id >= 0 && id < data.posts.length) {
         res.json({
