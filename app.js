783e4736407dc1bdd2d2f5f942ac3b14169dcd78
djragsdale <djragsdale@anderson.edu>
2015-11-18 23:01:57 -0500

    Fixed so it runs on IISNode

--- app.js
+++ app.js
@@ -25,9 +25,26 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(morgan('dev'));
+//app.use(express.logger('dev')); // Added to match default install.
 app.use(bodyParser());
+//app.use(express.bodyParser());
 app.use(methodOverride());
+//app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+*/
 
 var env = process.env.NODE_ENV || 'development';
 
---
+++ server.js
@@ -0,0 +1,67 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('error-handler'),
+  morgan = require('morgan'),
+  routes = require('./routes'),
+  //, user = require('./routes/user')
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(morgan('dev')); // from app.js
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(bodyParser());  // from app.js
+  //app.use(express.bodyParser());
+  app.use(methodOverride()); // from app.js
+  //app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+//var env = process.env.NODE_ENV || 'development';
+
+// development only
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+// production only
+app.configure('production', function () {
+    // TODO
+});
+
+
+/**
+ * Routes
+ */
+
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+//app.get('/users', user.list);
+
+
+// JSON API
+
+app.get('/api/posts', api.posts);
+
+app.get('/api/post/:id', api.post);
+app.post('/api/post', api.addPost);
+app.put('/api/post/:id', api.editPost);
+app.delete('/api/post/:id', api.deletePost);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
